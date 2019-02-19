da918c377bfb33acb99e8946fd66a008a2ed0a48
Jeffrey Yu <jwy5140@gmail.com>
2019-02-19 03:54:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(num)
   if (num%3 == 0 && num%5 == 0)
     return "FizzBuzz"
-  else if (num%3 == 0)
+  elsif (num%3 == 0)
     return "Fizz"
-  else if (num%5 == 0)
+  elsif (num%5 == 0)
     return "Buzz"
   else
     return NIL
